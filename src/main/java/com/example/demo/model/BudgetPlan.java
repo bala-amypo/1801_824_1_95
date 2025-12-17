5fe21d6ef7f17a6e95732c3ccd886760b3c3f49c
bala-amypo <bala@amypo.in>
2025-12-17 06:20:07 +0000

    User commit

--- src/main/java/com/example/demo/model/BudgetPlan.java
+++ src/main/java/com/example/demo/model/BudgetPlan.java
@@ -9,5 +9,23 @@ public class BudgetPlan{
     private Double incomeTarget;
     private Double expenseLimit;
       
+      public  void setId(int id){
+        this.id=id;
+      }
+      public int getId(){
+        return id;
+      }
+      public void setUser(String user){
+        this.user=user;
+      }
+      public String getUser(){
+        return user;
+      }
+      public  void setMonth(int month){
+        this.month=month;
+      }
+      public int getMonth(){
+        return month;
+      }
       
 }
