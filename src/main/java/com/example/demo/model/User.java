d41edbc5e8791116a1f4239aa3e1b4fb070020a6
bala-amypo <bala@amypo.in>
2025-12-18 05:45:23 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -22,6 +22,36 @@ public class User{
     public void setId(Long id){
         this.id=id;
     }
-    public String getName()
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getRole(){
+        return role;
+    }
+    public void setRole(String role){
+        this.role=role;
 
+    }
+    
+    public String getPass(){
+        return password;
+    }
+    public vois setPass(String password){
+        this.password=password;
+    }
+    public User(Long id,String email,String password,String role,String name){
+        this.id=id;
+        this.name=name;
+        this.role=role;
+        this.password=password;
+    }
 }
