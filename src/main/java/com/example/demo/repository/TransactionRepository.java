f645f0eb19f0d37724107dd2a8463e3f903aafd3
bala-amypo <bala@amypo.in>
2025-12-18 08:02:57 +0000

    User commit

--- src/main/java/com/example/demo/repository/TransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Transaction;
-
-public interface TransactionRepository extends JpaRepository<Transaction,Long>{
-      
-}
