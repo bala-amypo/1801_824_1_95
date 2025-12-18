ec6b461f5558f7b89da471ac66a386c17628ac90
bala-amypo <bala@amypo.in>
2025-12-18 04:05:42 +0000

    User commit

--- src/main/java/com/example/demo/model/BudgetSummary.java
+++ src/main/java/com/example/demo/model/BudgetSummary.java
@@ -2,7 +2,8 @@ package com.example.demo.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
-import jakarta 
+import jakarta.persistence.GeneratedValue;
+
 @Entity
 public class BudgetSummary{
     
