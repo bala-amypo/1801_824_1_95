37f45c2cbfc1f2c0666b2f4f4deaaaa8a6de06eb
bala-amypo <bala@amypo.in>
2025-12-18 13:08:46 +0000

    User commit

--- src/main/java/com/example/demo/service/BudgetSummaryService.java
+++ src/main/java/com/example/demo/service/BudgetSummaryService.java
@@ -1,7 +1,8 @@
 package com.example.demo.service;
 import jakarta.persistence.Service;
 import com.example.demo.model.BudgetSummary;
+@Service
 public interface BudgetSummaryService {
-    public BudgetSummary generateSummary(Long userId,BudgetPlan plan);
-    public BudgetSummary getSummary(Long userId,Integer month,Integer year);
+    public BudgetSummary generateSummary(Long budgetPlanId);
+    public BudgetSummary getSummary(Long budgetPlanId);
 }
