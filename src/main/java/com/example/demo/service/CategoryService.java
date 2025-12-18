24fffc442b1db024308972a3ac58377e92417516
bala-amypo <bala@amypo.in>
2025-12-18 12:59:41 +0000

    User commit

--- src/main/java/com/example/demo/service/CategoryService.java
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -1,1 +1,7 @@
-package com.example.demo.
+package com.example.demo.service;
+import jakarta.persistence.Service;
+import com.example.demo.model.Category;
+public interface CategoryService {
+    public Category addCategory(Category category);
+    public 
+}
