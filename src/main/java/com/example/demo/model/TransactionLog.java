7e761c0f9c6d700cfee9466f7a6e2bac7a8849a7
bala-amypo <bala@amypo.in>
2025-12-18 05:25:41 +0000

    User commit

--- src/main/java/com/example/demo/model/TransactionLog.java
+++ src/main/java/com/example/demo/model/TransactionLog.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
-import 
+import java.time.LocalDate;
 
 public class TransactionLog{
    @Id
@@ -15,6 +15,30 @@ public class TransactionLog{
    private String category;
    private Double amount;
    private String description;
-   private Localtime transactionDate;
+   private LocalDate transactionDate;
+
+   public Long getId(){
+    return id;
+   }
+   public void setId(Long id){
+    this.id=id;
+   }
+   public String getUser(){
+    return user;
+   }
+   public void setuser(String user){
+    this.user=user;
+   }
+   public String getCategory(){
+    return category;
+   }
+   public void setCategory(Strin category){
+    this.category=category;
+
+   }
+   public Double getAmount(){
+    return amount;
+   }
+   public void setamount(Double )
 
 }
