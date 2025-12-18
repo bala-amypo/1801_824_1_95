ceb858505faf11945ee17bcbad40fbc688183a2e
bala-amypo <bala@amypo.in>
2025-12-18 05:20:41 +0000

    User commit

--- src/main/java/com/example/demo/model/TransactionLog.java
+++ src/main/java/com/example/demo/model/TransactionLog.java
@@ -1,1 +1,20 @@
-// package com.example.demo.model
+
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import 
+
+public class TransactionLog{
+   @Id
+   @GeneratedValue(strategy=GenerationType.IDENTITY)
+   private Long id;
+   private String user;
+   private String category;
+   private Double amount;
+   private String description;
+   private Localtime transactionDate;
+
+}
