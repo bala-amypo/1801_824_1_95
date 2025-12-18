a5acdd11f58faccf23c330376c75e72af4c37fb1
bala-amypo <bala@amypo.in>
2025-12-18 03:55:41 +0000

    User commit

--- src/main/java/com/example/demo/model/BudgetSummary.java
+++ src/main/java/com/example/demo/model/BudgetSummary.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 public class BudgetSummary{
     
     @Id
+    @GeneratedValue(strategy=GeneratedKey
     private Long id;
     private String budgetPlan;
     private Double totalIncome;
