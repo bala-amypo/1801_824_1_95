2c8b60c2676a99d65ab4e2b5ef3d0a19cc961e6d
bala-amypo <bala@amypo.in>
2025-12-19 06:16:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Userimpl.java
+++ src/main/java/com/example/demo/service/impl/Userimpl.java
@@ -1,9 +1,16 @@
 package com.example.demo.service;
-
+import com.example.demo.repository.UserRepository;
 import com.example.demo.model.User;
- 
-public interface UserService{
-    public User register(User user)
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class Userimpl implements UserService{
+    
+    public User register(User user){
+     
+
+    }
 
     
     public User findByEmail(String email);
