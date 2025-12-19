53f32d49dee7d2ee29e90bfa518da6b0e7adebda
bala-amypo <bala@amypo.in>
2025-12-19 03:51:56 +0000

    User commit

--- src/main/java/com/example/demo/service/BudgetPlanService.java
+++ src/main/java/com/example/demo/service/BudgetPlanService.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
-import org.springframework
+
 import com.example.demo.model.BudgetPlan;
-@Service
+
 public interface BudgetPlanService {
     public BudgetPlan createBudgetPlan(Long userId,BudgetPlan plan);
     public BudgetPlan getBudgetPlan(Long userId,Integer month,Integer year);
