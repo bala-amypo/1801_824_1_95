2793e0efe039a314c4c89d10cf3a9cd7cd71e428
bala-amypo <bala@amypo.in>
2025-12-21 16:57:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/TransactionLogRepository.java
+++ src/main/java/com/example/demo/repository/TransactionLogRepository.java
@@ -6,3 +6,4 @@ import java.util.List;
 public interface TransactionLogRepository extends JpaRepository<TransactionLog,Long>{
       List<TransactionLog> findByUserId(Long id);
 }
+List<TemperatureSensorLog> findByshipmentId
