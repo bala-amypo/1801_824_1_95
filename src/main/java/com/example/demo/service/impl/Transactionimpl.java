9b5c85f0b33037789da2af10248646aba7adc2e0
bala-amypo <bala@amypo.in>
2025-12-21 12:36:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/Transactionimpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+import java.util.List; 
+import com.example.demo.model.TransactionLog;
+ 
+public interface TransactionService{
+    public TransactionLog addTransaction(Long userId,TransactionLog log);
+    public List<TransactionLog> getUserTransactions(Long userId);
+}
