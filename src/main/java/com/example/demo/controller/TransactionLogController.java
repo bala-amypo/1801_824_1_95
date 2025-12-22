ac3dbf5495075641ff3197a26056ca84d44b6745
bala-amypo <bala@amypo.in>
2025-12-22 06:55:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/TransactionLogController.java
+++ src/main/java/com/example/demo/controller/TransactionLogController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
- @RequestMapping("/transactions")
+@RequestMapping("/transactions")
 public class TransactionLogController {
 
     @Autowired
