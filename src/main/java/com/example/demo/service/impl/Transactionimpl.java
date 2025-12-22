dd4074860480880581d9747db13515d324c5e18b
bala-amypo <bala@amypo.in>
2025-12-22 05:34:11 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Transactionimpl.java
+++ src/main/java/com/example/demo/service/impl/Transactionimpl.java
@@ -1,20 +1,20 @@
-// package com.example.demo.service;
-// import java.util.List; 
-// import com.example.demo.repository.TransactionLogRepository;
-// import com.example.demo.model.TransactionLog;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// @Service
-// public class Transactionimpl implements TransactionService{
-//     @Autowired
-//     TransactionLogRepository tr;
-//     public TransactionLog addTransaction(String user,TransactionLog log){
-//          log.setUser(user);
-//        return tr.save(log);
+package com.example.demo.service;
+import java.util.List; 
+import com.example.demo.repository.TransactionLogRepository;
+import com.example.demo.model.TransactionLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class Transactionimpl implements TransactionService{
+    @Autowired
+    TransactionLogRepository tr;
+    public TransactionLog addTransaction(String user,TransactionLog log){
+         log.setUser(user);
+       return tr.save(log);
      
     
-//     }
-//     public List<TransactionLog> getUserTransactions(String user){
-//         return tr.findByUserId(user);
-// }
-// }
+    }
+    public List<TransactionLog> getUserTransactions(String user){
+        return tr.findByUserId(user);
+}
+}
