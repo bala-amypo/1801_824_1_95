75939834be12b152d90e040a49ca7717a2db6aad
bala-amypo <bala@amypo.in>
2025-12-22 05:07:36 +0000

    User commit

--- src/main/java/com/example/demo/controller/TransactionLogController.java
+++ src/main/java/com/example/demo/controller/TransactionLogController.java
@@ -1,33 +1,33 @@
-package com.example.demo.controller;
-import java.util.List;
-import com.example.demo.service.TransactionService;
-import com.example.demo.model.TransactionLog;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.beans.factory.annotation.Autowired;
+// package com.example.demo.controller;
+// import java.util.List;
+// import com.example.demo.service.TransactionService;
+// import com.example.demo.model.TransactionLog;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.beans.factory.annotation.Autowired;
 
-@RestController
+// @RestController
  
-public class TransactionLogController {
+// public class TransactionLogController {
 
-    @Autowired
-    TransactionService ts;
+//     @Autowired
+//     TransactionService ts;
 
      
-    @PostMapping("/{user}")
-    public TransactionLog addTransaction(
-            @PathVariable String user,
-            @RequestBody TransactionLog log) {
+//     @PostMapping("/{user}")
+//     public TransactionLog addTransaction(
+//             @PathVariable String user,
+//             @RequestBody TransactionLog log) {
 
-        return ts.addTransaction(user, log);
-    }
+//         return ts.addTransaction(user, log);
+//     }
 
     
-    @GetMapping("/{user}")
-    public List<TransactionLog> getAll(@PathVariable String user) {
-        return ts.getUserTransactions(user);
-    }
-}
+//     @GetMapping("/{user}")
+//     public List<TransactionLog> getAll(@PathVariable String user) {
+//         return ts.getUserTransactions(user);
+//     }
+// }
--- src/main/java/com/example/demo/service/TransactionService.java
+++ src/main/java/com/example/demo/service/TransactionService.java
@@ -1,8 +1,8 @@
-package com.example.demo.service;
-import java.util.List; 
-import com.example.demo.model.TransactionLog;
+// package com.example.demo.service;
+// import java.util.List; 
+// import com.example.demo.model.TransactionLog;
  
-public interface TransactionService{
-    public TransactionLog addTransaction(String user,TransactionLog log);
-    public List<TransactionLog> getUserTransactions(String user);
-}
+// public interface TransactionService{
+//     public TransactionLog addTransaction(String user,TransactionLog log);
+//     public List<TransactionLog> getUserTransactions(String user);
+// }
