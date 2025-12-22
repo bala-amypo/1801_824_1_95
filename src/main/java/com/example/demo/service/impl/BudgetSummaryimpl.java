8f8ff1734ccf12876a02f205ca98509f67be20fe
bala-amypo <bala@amypo.in>
2025-12-22 08:50:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/BudgetSummaryimpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+ import java.util.List;
+import com.example.demo.model.BudgetSummary;
+ import com.example.demo.repository.BudgetSummaryRepository;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import com.example.demo.service.BudgetSummaryService;
+ import org.springframework.stereotype.Service;
+ @Service
+import com.example.demo.model.BudgetSummary;
+ 
+public class BudgetSummarimpl implements BudgetSummaryService {
+    public BudgetSummary generateSummary(Long budgetPlanId){
+
+    }
+    public BudgetSummary getSummary(Long budgetPlanId){
+
+}
