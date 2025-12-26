0e29bc3a8cf7a12119287cec6a9a5855559e1932
bala-amypo <bala@amypo.in>
2025-12-26 09:22:02 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,22 +1,15 @@
 package com.example.demo.dto;
 
-public class RegisterRequest {
+public class LoginRequest {
 
-    private String name;
     private String email;
     private String password;
 
-    public RegisterRequest() {}
+    public LoginRequest() {}
 
-    public String getName() {
-        return name;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,4 +1,4 @@
- package com.example.demo.dto;
+package com.example.demo.dto;
 
 public class RegisterRequest {
 
@@ -8,15 +8,12 @@ public class RegisterRequest {
 
     public RegisterRequest() {}
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getEmail() {
-        return email;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public String getPassword() {
-        return password;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
