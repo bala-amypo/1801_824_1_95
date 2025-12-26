2d4e2b7aa9addaa0e6e4ae583252854af25800b6
bala-amypo <bala@amypo.in>
2025-12-26 09:07:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -9,12 +9,11 @@
 // }
 package com.example.demo.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-    // used to check duplicate category
-    boolean existsByName(String name);
+    Optional<Category> findByName(String name);
 }
+
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -9,16 +9,10 @@
 package com.example.demo.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    // required by UserService
     Optional<User> findByEmail(String email);
-
-    // used to avoid duplicate registration
-    boolean existsByEmail(String email);
 }
+
