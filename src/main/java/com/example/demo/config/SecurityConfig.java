0a17e03dca3151fd6fddad2bd96a7ea3bc9c40c5
bala-amypo <bala@amypo.in>
2025-12-29 09:25:43 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -16,7 +16,7 @@
 // }
 
 package com.example.demo.config;
-
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
